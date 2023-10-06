ceb1d23f4b030a8882bf557c79072545bda9397d
Anshuman Chakraborty <111342412+Anshuman-Chakraborty@users.noreply.github.com>
2023-10-06 18:50:59 +0530

    Java v for Remove_duplicate_element_from_sorted_Linked_List

---
+++ Remove_duplicate_element_from_sorted_linked_list/code.java
@@ -0,0 +1,24 @@
+/*
+class Node
+    {
+        int data;
+        Node next;
+        Node(int d) {data = d; next = null; }
+    }
+*/
+
+class GfG
+{
+    //Function to remove duplicates from sorted linked list.
+    Node removeDuplicates(Node head)
+    {
+	// Your code here	
+	    Node node = head;
+	    
+	    while(node!=null && node.next!=null){
+	        if(node.data==node.next.data) node.next = node.next.next;
+	        else node = node.next;
+	    }
+	    return head;
+    }
+}
